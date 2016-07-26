95c739937f603ab328a1b22c44b80381aaedf432
Mathieu JULIE <mathieujulie974@hotmail.com>
2016-07-26 09:15:20 +0400

    fosuser + crud

---
+++ src/LeBonZafer/BlogBundle/Form/ArticleType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace LeBonZafer\BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+
+class ArticleType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('titre')
+            ->add('description')
+            ->add('dateCreation', DatetimeType::class)
+            ->add('brouillon')
+            ->add('admin')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LeBonZafer\BlogBundle\Entity\Article'
+        ));
+    }
+}
